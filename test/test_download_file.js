6853feb5e7d61905dbae51cfa73e968ec316c465
kimtn-bitmark <kim@bitmark.com>
2015-10-12 05:26:41 +0700

    Update test file

--- test/test_download_file.js
+++ test/test_download_file.js
@@ -4,7 +4,8 @@ var path = require('path');
 var session = new libtorrent.Session();
 
 var savepath = path.resolve(__dirname);
-var magnet_uri = 'magnet:?xt=urn:btih:61e7f76315bb2fbd93962a830b7725ea639763ea&dn=precise64.box';
+// var magnet_uri = 'magnet:?xt=urn:btih:61e7f76315bb2fbd93962a830b7725ea639763ea&dn=precise64.box';
+var magnet_uri = 'magnet:?xt=urn:btih:82d05efa9a34bcb143215c307a6f066179551000&dn=test_data.txt';
 var th_ptr = session.add_torrent_by_maget_uri(savepath, magnet_uri);
 
 var time = setInterval(function() {
