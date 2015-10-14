612a0958582ea7ea35d74350dcd95b73bb61dbef
kimtn-bitmark <kim@bitmark.com>
2015-10-14 11:29:01 +0700

    fix minor

--- src/js/libtorrent.js
+++ src/js/libtorrent.js
@@ -2,21 +2,22 @@ var ffi = require('ffi');
 var ref = require('ref');
 var path = require('path');
 
+// get root dir
+var root_dir = __dirname.replace('/src/js', '/');
+var boostSystem, boostThread, libtorrentRasterbar;
+
+// Load boost library and libtorrent library
+if (ffi.LIB_EXT == '.so') {
+  boostSystem = ffi.DynamicLibrary(root_dir + 'lib/linux/libboost_system' + ffi.LIB_EXT + '.1.54.0');
+  boostThread = ffi.DynamicLibrary(root_dir + '/lib/linux/libboost_thread' + ffi.LIB_EXT + '.1.54.0');
+  libtorrentRasterbar = ffi.DynamicLibrary(root_dir + '/lib/linux/libtorrent-rasterbar' + ffi.LIB_EXT + '.8');
+} else if(ffi.LIB_EXT == '.dylib') {
+  boostSystem = ffi.DynamicLibrary(root_dir + '/lib/osx/libboost_system' + ffi.LIB_EXT);
+  boostThread = ffi.DynamicLibrary(root_dir + '/lib/osx/libboost_thread' + ffi.LIB_EXT);
+  libtorrentRasterbar = ffi.DynamicLibrary(root_dir + '/lib/osx/libtorrent-rasterbar.8' + ffi.LIB_EXT);
+}
+
 module.exports = function() {
-
-  // get root dir
-  var root_dir = __dirname.replace('/src/js', '/');
-
-  // Load boost library and libtorrent library
-  if (ffi.LIB_EXT == '.so') {
-    var boostSystem = ffi.DynamicLibrary(root_dir + 'lib/ubuntu/libboost_system' + ffi.LIB_EXT + '.1.54.0');
-    var boostThread = ffi.DynamicLibrary(root_dir + '/lib/ubuntu/libboost_thread' + ffi.LIB_EXT + '.1.54.0');
-    var libtorrentRasterbar = ffi.DynamicLibrary(root_dir + '/lib/ubuntu/libtorrent-rasterbar' + ffi.LIB_EXT + '.8');
-  } else if(ffi.LIB_EXT == '.dylib') {
-    var boostSystem = ffi.DynamicLibrary(root_dir + '/lib/mac/libboost_system' + ffi.LIB_EXT);
-    var boostThread = ffi.DynamicLibrary(root_dir + '/lib/mac/libboost_thread' + ffi.LIB_EXT);
-    var libtorrentRasterbar = ffi.DynamicLibrary(root_dir + '/lib/mac/libtorrent-rasterbar.8' + ffi.LIB_EXT);
-  }
 
   var s = ffi.Library(path.resolve(root_dir, 'src/cpp/session'), {
     'get_session_ptr': [ 'pointer', [] ],
