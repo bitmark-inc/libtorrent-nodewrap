74ca1ab11934186ec68706968165226902da628b
kimtn-bitmark <kim@bitmark.com>
2015-10-28 16:12:21 +0700

    - Add GeoIP, JSON library - Fix some minor bugs on MAC

--- src/cpp/torrent_handle.cpp
+++ src/cpp/torrent_handle.cpp
@@ -6,13 +6,13 @@ using namespace libtorrent;
 
 extern "C" {
 
-  char* convert_2_hex_string(unsigned char *data, int len) {
+  std::string convert_2_hex_string(unsigned char *data, int len) {
     std::stringstream ss;
     ss << std::hex;
     for (int i = 0; i < len; ++i) {
       ss << std::setw(2) << std::setfill('0') << (int)data[i];
     }
-    return (char*)ss.str().c_str();
+    return ss.str();
   }
   bool is_valid(torrent_handle* handle) {
     return handle->is_valid();
@@ -21,7 +21,8 @@ extern "C" {
     std::cout << "Inside function::::" << handle->info_hash() << std::endl;
     unsigned char* info_hash = (unsigned char*)handle->info_hash().to_string().c_str();
     int lng = strlen((char *)info_hash);
-    return convert_2_hex_string(info_hash, lng);
+    std::string tmp = convert_2_hex_string(info_hash, lng);
+    return (char*)tmp.c_str();
   }
   torrent_status* status(torrent_handle* handle) {
     return new torrent_status(handle->status());
