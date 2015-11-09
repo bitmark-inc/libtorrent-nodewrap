24076f95f2e2ed71f6593a7710806881246ab571
kimtn-bitmark <kim@bitmark.com>
2015-11-09 08:46:19 +0700

    Add json and curl library for linux

--- src/js/load_external_library.js
+++ src/js/load_external_library.js
@@ -3,11 +3,13 @@ var path = require('path');
 
 // get root dir
 var root_dir = __dirname.replace('/src/js', '/');
-var boostSystem, boostThread, libtorrentRasterbar;
+var boostSystem, boostThread, libtorrentRasterbar, libJson, libCurl;
 
 // Load boost library and libtorrent library
 if (ffi.LIB_EXT === '.so') {
   boostSystem = ffi.DynamicLibrary(root_dir + 'lib/linux/libboost_system' + ffi.LIB_EXT + '.1.54.0');
   boostThread = ffi.DynamicLibrary(root_dir + 'lib/linux/libboost_thread' + ffi.LIB_EXT + '.1.54.0');
   libtorrentRasterbar = ffi.DynamicLibrary(root_dir + 'lib/linux/libtorrent-rasterbar' + ffi.LIB_EXT + '.8');
+  libJson = ffi.DynamicLibrary(root_dir + 'lib/linux/libjson' + ffi.LIB_EXT);
+  libCurl = ffi.DynamicLibrary(root_dir + 'lib/linux/libcurl' + ffi.LIB_EXT);
 }
